8bec4309402ba2949c49ea58fafea51d7054cf27
bossclement <122975092+bossclement@users.noreply.github.com>
2025-06-18 15:57:12 +0200

    Update bumpsInRoad.ts

--- bumpsInRoad.ts
+++ bumpsInRoad.ts
@@ -2,3 +2,8 @@
 Author: Bossclement
 source: https://www.codewars.com/kata/57ed30dde7728215300005fa
 */
+
+export function bump(x: string): string{
+  const bumps = x.split('').filter(char => char === 'n').length;
+  return bumps <= 15 ? 'Woohoo!' : 'Car Dead';
+}
